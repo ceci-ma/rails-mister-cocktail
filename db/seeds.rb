f73c6b8fa657c2d051c6b35b979edfeeb1152f47
Cecilia Marcellesi <ceciliamarcellesi@gmail.com>
2019-11-15 10:39:48 +0000

    Mr Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "angostura bitters")
+Ingredient.create(name: "sugar syrup")
+
+Cocktail.create(name: "Walk of shame")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Old Fashioned")
+
